eadcdcaa6ecea990e400f671230e1b63d7fd0cfb
Erik Fears <git@strtok.net>
2010-10-10 10:35:10 -0700

    add util.go

---
+++ util.go
@@ -0,0 +1,24 @@
+package main
+
+import "io"
+import "net/textproto"
+
+func ReadLineIter(conn io.ReadWriteCloser) chan string {
+ 
+   ch := make(chan string)
+   textConn := textproto.NewConn(conn)
+   
+   go func() {
+      for {
+         line, err := textConn.ReadLine()
+         
+         if err != nil {
+            break
+         }
+         ch <- line
+      }
+      close(ch)
+   }()
+   
+   return ch
+}
